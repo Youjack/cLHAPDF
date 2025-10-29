3f7a096f165d17a0f3b769035dbc4de2e0e576f6
Youjack <iamjasonlin@163.com>
2025-10-29 14:55:58 +0800

    add support for LHAPDF exceptions

--- src/cLHAPDF.jl
+++ src/cLHAPDF.jl
@@ -8,12 +8,19 @@ function __init__()
     push!(DL_LOAD_PATH, joinpath(@__DIR__, "..", "lib"))
 end
 
+function check_null_pointer(ptr::Ptr{Cvoid})
+    if ptr == C_NULL
+        throw(ArgumentError("null pointer encountered"))
+    end
+end
+
 function getPDFSet(setname::AbstractString)::Ptr{Cvoid}
     pdfset = @ccall "libclhapdf".getPDFSet(setname::Cstring)::Ptr{Cvoid}
     return pdfset
 end
 
 function get_entry(pdfset::Ptr{Cvoid}, key::AbstractString)::String
+    check_null_pointer(pdfset)
     val = @ccall "libclhapdf".get_entry(pdfset::Ptr{Cvoid}, key::Cstring)::Cstring
     return unsafe_string(val)
 end
@@ -25,9 +32,11 @@ function mkPDF(setname_nmem::AbstractString)::Ptr{Cvoid}
 end
 
 function alphasQ2(pdf::Ptr{Cvoid}, Q2::Real):: Float64
+    check_null_pointer(pdf)
     @ccall "libclhapdf".alphasQ2(pdf::Ptr{Cvoid}, Q2::Cdouble)::Cdouble
 end
 function xfxQ2(pdf::Ptr{Cvoid}, pid::Integer, x::Real, Q2::Real):: Float64
+    check_null_pointer(pdf)
     @ccall "libclhapdf".xfxQ2(pdf::Ptr{Cvoid}, pid::Int, x::Cdouble, Q2::Cdouble)::Cdouble
 end
 
--- src/clhapdf.cpp
+++ src/clhapdf.cpp
@@ -2,23 +2,43 @@
 #include "clhapdf.h"
 #include <string>
 #include <iostream>
+#include <math.h>
 using namespace std;
+
+void print_lhapdf_exception(const exception& e) {
+    cerr << "\033[1;31mLHAPDF Exception:\033[0m " << e.what() << endl;
+}
 
 extern "C" {
 
 cPDFSet* getPDFSet(const char* setname) {
-    LHAPDF::PDFSet& pdfset = LHAPDF::getPDFSet(string(setname));
-    return reinterpret_cast<cPDFSet*>(&pdfset);
+    try {
+        LHAPDF::PDFSet& pdfset = LHAPDF::getPDFSet(string(setname));
+        return reinterpret_cast<cPDFSet*>(&pdfset);
+    } catch (const exception& e) {
+        print_lhapdf_exception(e);
+        return nullptr;
+    }
 }
 
 const char* get_entry(cPDFSet* cpdfset, const char* key) {
     LHAPDF::PDFSet* pdfset = reinterpret_cast<LHAPDF::PDFSet*>(cpdfset);
-    return pdfset->get_entry(string(key)).c_str();
+    try {
+        return pdfset->get_entry(string(key)).c_str();
+    } catch (const exception& e) {
+        print_lhapdf_exception(e);
+        return nullptr;
+    }
 }
 
 cPDF* mkPDF(const char* setname_nmem) {
-    LHAPDF::PDF* pdf = LHAPDF::mkPDF(string(setname_nmem));
-    return reinterpret_cast<cPDF*>(pdf);
+    try {
+        LHAPDF::PDF* pdf = LHAPDF::mkPDF(string(setname_nmem));
+        return reinterpret_cast<cPDF*>(pdf);
+    } catch (const exception& e) {
+        print_lhapdf_exception(e);
+        return nullptr;
+    }
 }
 void rmPDF(cPDF* cpdf) {
     LHAPDF::PDF* pdf = reinterpret_cast<LHAPDF::PDF*>(cpdf);
@@ -27,11 +47,21 @@ void rmPDF(cPDF* cpdf) {
 
 double alphasQ2(cPDF* cpdf, double Q2) {
     LHAPDF::PDF* pdf = reinterpret_cast<LHAPDF::PDF*>(cpdf);
-    return pdf->alphasQ2(Q2);
+    try {
+        return pdf->alphasQ2(Q2);
+    } catch (const exception& e) {
+        print_lhapdf_exception(e);
+        return NAN;
+    }
 }
 double xfxQ2(cPDF* cpdf, int pid, double x, double Q2) {
     LHAPDF::PDF* pdf = reinterpret_cast<LHAPDF::PDF*>(cpdf);
-    return pdf->xfxQ2(pid, x, Q2);
+    try {
+        return pdf->xfxQ2(pid, x, Q2);
+    } catch (const exception& e) {
+        print_lhapdf_exception(e);
+        return NAN;
+    }
 }
 
 } // extern "C"
